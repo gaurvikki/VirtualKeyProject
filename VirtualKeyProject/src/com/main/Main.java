57e82f912b12b5756e153c960db1daae0fc1987d
Prave <Prave@LAPTOP-JK44MJAO>
2022-10-05 14:57:47 +0530

    another commit to repo.

--- VirtualKeyProject/src/com/main/Main.java
+++ VirtualKeyProject/src/com/main/Main.java
@@ -8,7 +8,7 @@ import com.main.service.Operation;
 import com.main.service.UserOperations;
 
 public class Main {
-
+//
 	public static void main(String[] args) {
 		Display display = new Display();
 		Operation operation = new Operation();
@@ -16,25 +16,27 @@ public class Main {
 		try {
 			int val1 = 1;
 			while (val1 != 0) {
-
+				System.out.print("\u000c");
+				System.out.print("\033[H\033[2J");
 				display.displayHomePage();
 				Scanner myObj = new Scanner(System.in);
 				int val2 = myObj.nextInt();
 
 				switch (val2) {
-				case 1:
+				case 1: System.out.print("\u000c");
 					operation.printDirectory();
 					break;
-				case 2: userOperations.doOperations();
+				case 2:System.out.print("\u000c"); 
+					userOperations.doOperations();
 					break;
-				case 3:
+				case 3:System.out.print("\u000c");
 					System.out.println("Closing the Application: Bye..."); val1=0;
 					break;
 				default:
 					System.out.println("You entered a wrong choice, please enter correct value");
 					break;
 				}
-//				System.out.flush();
+				System.out.print("\u000c");
 			}
 		} catch (InputMismatchException e) {
 			System.out.println("Input value is not correct");
